781d175347323e53f92be57fc1346d977c79a6e9
sukanyapavuluri33 <101783413+sukanyapavuluri33@users.noreply.github.com>
2022-09-10 08:04:06 -0700

    Create set.py

---
+++ set.py
@@ -0,0 +1,11 @@
+li={1,2,3,4}
+li1={3,4,5,6}
+uni=li|li1
+print("UNION IS:",uni)
+inter=li&li1
+print("INTERSECTION IS:",inter)
+dif=li-li1
+print("DIFFERENCE IS:",dif)
+sym=li^li1
+print("SYMMETRIC DIFFERENCE IS: ",sym)
+
